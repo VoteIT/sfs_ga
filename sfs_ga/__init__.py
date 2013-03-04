ed4ffdc098c5e56320e676dfa23854ff506f1cbc
robinharms <robin@betahaus.net>
2013-03-04 09:48:13 +0100

    Register css

--- sfs_ga/__init__.py
+++ sfs_ga/__init__.py
@@ -10,3 +10,9 @@ def includeme(config):
     config.add_translation_dirs('%s:locale/' % PROJECTNAME)
     cache_ttl_seconds = int(config.registry.settings.get('cache_ttl_seconds', 7200))
     config.add_static_view('sfs_ga_static', '%s:static' % PROJECTNAME, cache_max_age = cache_ttl_seconds)
+
+    #Register fanstatic resources
+    from voteit.core.models.interfaces import IFanstaticResources
+    from .fanstaticlib import sfs_styles
+    util = config.registry.getUtility(IFanstaticResources)
+    util.add('sfs_styles', sfs_styles)
