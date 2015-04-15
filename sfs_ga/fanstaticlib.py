547a65342ff1d60554083e37c1029ed705202fb7
robinharms <robin@betahaus.net>
2015-04-15 12:50:40 +0200

    Cleanup

--- sfs_ga/fanstaticlib.py
+++ sfs_ga/fanstaticlib.py
@@ -1,17 +1,14 @@
-from arche.interfaces import IViewInitializedEvent
+from arche.fanstatic_lib import common_js
 from arche.interfaces import IBaseView
+from arche.interfaces import IViewInitializedEvent
 from fanstatic import Library
 from fanstatic import Resource
-
 from voteit.core.fanstaticlib import voteit_main_css
-from arche.fanstatic_lib import common_js
-#from voteit.core.fanstaticlib import voteit_common_js
 
 sfs_ga_lib = Library('sfs_ga_lib', 'static')
 
 sfs_styles = Resource(sfs_ga_lib, 'styles.css', depends = (voteit_main_css,))
 sfs_manage_delegation = Resource(sfs_ga_lib, 'manage_delegation.js', depends = (common_js,))
-sfs_delegations = Resource(sfs_ga_lib, 'delegations.js', depends = ())
 
 
 def need_sfs(view, event):
