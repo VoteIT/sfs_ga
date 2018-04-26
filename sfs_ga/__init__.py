c226ef3e7ff23410d3f009c0bb1df005be566f4a
Robin Harms Oredsson <robin@betahaus.net>
2018-04-26 13:56:45 +0200

    Not used

--- sfs_ga/__init__.py
+++ sfs_ga/__init__.py
@@ -1,7 +1,6 @@
 import logging
 
 from pyramid.i18n import TranslationStringFactory
-from voteit.core.models.interfaces import IJSUtil
 
 
 PROJECTNAME = 'sfs_ga'
@@ -15,23 +14,12 @@ def includeme(config):
     config.add_translation_dirs('%s:locale/' % PROJECTNAME)
     cache_max_age = int(config.registry.settings.get('arche.cache_max_age', 60*60*24))
     config.add_static_view('sfs_ga_static', '%s:static' % PROJECTNAME, cache_max_age = cache_max_age)
-
     config.include('.fanstaticlib')
     config.include('.models')
     config.include('.schemas')
     config.include('.utils')
     config.include('.views')
     config.include('.subscribers')
-
-    #Register js translations
-    js_util = config.registry.queryUtility(IJSUtil)
-    if js_util:
-        js_util.add_translations(
-            supporters = _(u"Supporters"),
-            )
-    else:
-        log.warn("IJSUtil util not found, this might be okay if you're just running a test")
-
     #Remove like action
     from betahaus.viewcomponent import IViewGroup
     vg_user_tags = config.registry.queryUtility(IViewGroup, name = 'user_tags')
